633de06b09655de9768ba6e558d45fdbec73402c
ahmed5163 <ahmed5163@gmail.com>
2021-07-17 23:32:20 +0300

    First Push

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+﻿document.getElementById("NewArt").style.display = "none";
+
+function NewArt() {
+    document.getElementById("emp").style.display = "none";
+    document.getElementById("NewArt").style.display = "block";
+    
+}
+function NWArt() {
+    
+
+}
