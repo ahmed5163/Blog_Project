2682c0b8e47fd5a95c478ee0eace693269a59372
ahmed5163 <ahmed5163@gmail.com>
2021-07-18 20:32:33 +0300

    adding articels

--- JavaScript.js
+++ JavaScript.js
@@ -7,5 +7,24 @@ function NewArt() {
 }
 function NWArt() {
     
+    var myDiv = document.createElement("div");
 
+    var title = document.getElementById("Name").value
+    var cont = document.getElementById("cont").value
+
+    if (title.length > 0) {
+        myDiv.id = 'div_id';
+
+
+        myDiv.innerHTML = "<h1>" + title + "</h1> <br> <p>" + cont+"</p>";
+
+
+        document.body.appendChild(myDiv);
+        document.getElementById("NewArt").style.display = "none";
+
+    } else {
+
+        alert("The title cannot be empty")
+    }
+  
 }
